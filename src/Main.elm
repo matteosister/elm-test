bf21c6210394d56a61c598f884a9592d17995b03
Vitalij Zadneprovskij <vitalij_zad@libero.it>
2020-05-22 15:01:20 +0200

    Piccoli esercizi

--- src/Main.elm
+++ src/Main.elm
@@ -1,8 +1,8 @@
 module Main exposing (..)
 
 import Browser exposing (sandbox)
-import Html exposing (Html, button, div)
-import Html.Events exposing (onClick)
+import Html exposing (Html, button, div, input, hr, text)
+import Html.Events exposing (onClick, onInput)
 
 
 
@@ -14,16 +14,21 @@ import Html.Events exposing (onClick)
 type Msg
     = Increase
     | Decrease
+    | ChangeIncreaseAmount String
+    | ChangeTextField String
 
 
 type alias Model =
-    { counterValue : Int }
+    { 
+        counterValue : Int,
+        increaseAmount: Int,
+        textFieldContent: String
+     }
 
 
 initialModel : Int -> Model
 initialModel value =
-    Model value
-
+    Model value 0 ""
 
 main : Program () Model Msg
 main =
@@ -38,15 +43,31 @@ update : Msg -> Model -> Model
 update msg model =
     case msg of
         Increase ->
-            { model | counterValue = model.counterValue + 1 }
+            { model | counterValue = model.counterValue + model.increaseAmount }
 
         Decrease ->
             { model | counterValue = model.counterValue - 1 }
 
+        ChangeIncreaseAmount amount -> 
+            case String.toInt amount of
+                Just i -> { model | increaseAmount = i }
+                Nothing -> model
+
+        ChangeTextField value ->
+            {model | textFieldContent = value}
 
 view : Model -> Html Msg
 view model =
     div []
-        [ Html.text (String.fromInt model.counterValue)
-        , button [ onClick Increase ] [ Html.text "increase" ]
+        [ div [] [text (String.fromInt model.counterValue)]
+        , div [] [
+            input [onInput ChangeIncreaseAmount][], 
+            button [ onClick Increase ] [ text "increase" ]]
+        , div [] [button [ onClick Decrease ] [ text "decrease" ]]
+        , hr[][]
+        , div [] [
+            text model.textFieldContent]
+        , div [] [
+            input [onInput ChangeTextField][]
+        ]
         ]
